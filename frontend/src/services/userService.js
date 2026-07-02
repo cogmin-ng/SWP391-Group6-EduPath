ffd581a1ee2ec08cd07ddc7d8a883f8d32ce2713
dangrangto06 <minhdang1123344@gmail.com>
2026-07-02 10:20:53 +0700

    fix frontend for viewing roadmap for mentor + fix the mentor's profile

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,7 +2,7 @@ import api from "./api";
 
 export const userService = {
   async updateUser(userId, payload) {
-    const { data } = await api.patch(`/users/${userId}`, payload);
+    const { data } = await api.put(`/users/${userId}`, payload);
     return data;
   },
 
